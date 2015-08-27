647587297ff6972d53cf0b20c4ba4d25e2f0c600
Yu Hayakawa <yu.v.xix@gmail.com>
2015-08-27 17:21:57 +0900

    inviterから依頼を受けた際に，invitee（友人）にpush通知を送るように実装

--- Server/campus_de_lunch/app/api/api.rb
+++ Server/campus_de_lunch/app/api/api.rb
@@ -13,14 +13,42 @@ module API
     end
 
     # とりあえずのユーザデータ
-    users = {"aki" => 1, "obata" => 2, "banri" => 3, "moriya" => 4, "haya" => 5}
+    # users = {"aki" => 1, "obata" => 2, "banri" => 3, "moriya" => 4, "haya" => 5}
+    # tokens = {
+    #   "aki" => "a88b829f5cb810462640764dc599f82e80513123601f6c70a115e1ab832cc18c",
+    #   "obata" => "token",
+    #   "banri" => "token",
+    #   "moriya" => "token",
+    #   "haya" => "token"
+    # }
+    users = {"aki" => 1, "obata" => 2, "moriya" => 3}
+    tokens = {
+      "aki" => "a88b829f5cb810462640764dc599f82e80513123601f6c70a115e1ab832cc18c",
+      "obata" => "f6e0504c03f3bd83a626f1c4e7fd7195ac70206197f274f4f51f28b787d59ee3",
+      "moriya" => "ab1a33f9768fff4c77313d860487908036fb3eff3cb0a73828ccad9653a26f90"
+    }
+
 
     # STEP1
     # 誘う側がサーバにリクエストをした際の動作を記述する．
     array=[]
     post :confirm do
+
       users.each{|key, value|
         if key != params[:inverter]
+
+          APN = Houston::Client.development
+          APN.certificate = File.read("/home/vagrant/Server/campus_de_lunch/config/server_certificates_sandbox.pem")
+
+          notification = Houston::Notification.new(device: tokens[key])
+          notification.sound = ''
+          notification.alert = "Hello, World!"
+          notification.content_available = true
+          notification.custom_data = {inviter: params[:inverter], invitee: key, status: ""}
+
+          APN.push(notification)
+          puts "Error: #{notification.error}." if notification.error
+
           array.push(key)
         end
       }
@@ -32,27 +60,17 @@ module API
     #サイレントpush通知(iOS)
 
     get :silentios do
-      # Environment variables are automatically read, or can be overridden by any specified options. You can also
-      # conveniently use `Houston::Client.development` or `Houston::Client.production`.
       APN = Houston::Client.development
       APN.certificate = File.read("/home/vagrant/Server/campus_de_lunch/config/server_certificates_sandbox.pem")
-      # APN.certificate = File.read(Dir["#{Rails.root}/config/server_certificates_sandbox.pem"])
 
-      # An example of the token sent back when a device registers for notifications
       token = "a88b829f5cb810462640764dc599f82e80513123601f6c70a115e1ab832cc18c"
 
-      # Create a notification that alerts a message to the user, plays a sound, and sets the badge on the app
       notification = Houston::Notification.new(device: token)
-      notification.alert = "Hello, World!"
+      notification.sound = ''
 
-      # Notifications can also change the badge count, have a custom sound, have a category identifier, indicate available Newsstand content, or pass along arbitrary data.
-      notification.badge = 10000
-      notification.sound = "sosumi.aiff"
-      notification.category = "INVITE_CATEGORY"
       notification.content_available = true
-      notification.custom_data = {foo: "bar"}
+      notification.custom_data = {aki: true}
 
-      # And... sent! That's all it takes.
       APN.push(notification)
       puts "Error: #{notification.error}." if notification.error
     end
@@ -67,24 +85,6 @@ module API
       # return {"return" => 1, "invitee" => "aki", "status" => true}
       return {"invitee" => "aki", "status" => true}
     end
-
-    # resource :memos do
-    #   get do
-    #     Memo.all
-    #   end
-    #
-    #   get ':id' do
-    #       Memo.find(params[:id])
-    #   end
-    #
-    #   post do
-    #     Memo.create(
-    #       :title => params[:title],
-    #       :body => params[:body],
-    #       :author => params[:author]
-    #     )
-    #   end
-    # end
 
   end
 end
