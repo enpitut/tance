e78784a693e394ab429fef9ce9193876713c3173
Banri Nakamura <uiyatsudesu@gmail.com>
2015-08-27 12:54:06 +0900

    third

--- Android/PBLtsukuba2/app/src/main/java/com/example/banrinakamura/pbltsukuba2/GcmBroadcastReceiver.java
+++ Android/PBLtsukuba2/app/src/main/java/com/example/banrinakamura/pbltsukuba2/GcmBroadcastReceiver.java
@@ -14,8 +14,12 @@ public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        ComponentName comp =
-                new ComponentName(context.getPackageName(), GcmIntentService.class.getName());
+
+        // Explicitly specify that GcmMessageHandler will handle the intent.
+        ComponentName comp = new ComponentName(context.getPackageName(),
+                GcmIntentService.class.getName());
+
+        // Start the service, keeping the device awake while it is launching.
         startWakefulService(context, (intent.setComponent(comp)));
         setResultCode(Activity.RESULT_OK);
     }
