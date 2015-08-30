fccfa6eedb31bdfbfd9e636ce4e2d9b99bebf66d
Yu Hayakawa <s1520679@u.tsukuba.ac.jp>
2015-08-30 18:30:06 +0900

    プロトタイプ完成版

--- Server/campus_de_lunch/app/api/api.rb
+++ Server/campus_de_lunch/app/api/api.rb
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
 require 'houston'
 require 'gcm'
+
 module API
   class Base < Grape::API
     format :json
@@ -14,24 +16,21 @@ module API
 
     # STEP1,2
     # 誘う側がサーバにリクエストをした際の動作を記述する．
-    array=[]
     post :confirm do
       tokens.each{|key, value|
         if key != params[:inviter]
           if key != "banri"
             APN = Houston::Client.development
-            APN.certificate = File.read("/home/vagrant/Server/campus_de_lunch/config/server_certificates_sandbox.pem")
+            APN.certificate = File.read("/root/Server/campus_de_lunch/config/server_certificates_sandbox.pem")
 
             notification = Houston::Notification.new(device: tokens[key])
             notification.sound = ''
-            notification.alert = "STEP2: Detected Confirm. FROM: #{params[:inviter]} TO: #{key}"
+#            notification.alert = "STEP2: Detected Confirm. FROM: #{params[:inviter]} TO: #{key}"
             notification.content_available = true
             notification.custom_data = {inviter: params[:inviter], invitee: key, status: ''}
 
             APN.push(notification)
             puts "Error: #{notification.error}." if notification.error
-
-            array.push(key)
 
           else
             api_key = "AIzaSyBzrhYXPn3w0fxbARde28W9Z8rg6FI2J_M"
@@ -39,40 +38,54 @@ module API
             gcm = GCM.new(api_key)
             options = {data: {inviter: params[:inviter], invitee: key, status: ''}, collapse_key: "updated_score"}
             response = gcm.send_notification(registration_ids, options)
-            array.push(key)
           end
-
         end
       }
-
-      return array
     end
 
-
+    statusArray =[]
     # STEP3,4
     # 誘われた側が，サーバに状態を返答した際の動作を記述する．
     post :reply do
       if params[:inviter] != "banri"
 
         APN = Houston::Client.development
-        APN.certificate = File.read("/home/vagrant/Server/campus_de_lunch/config/server_certificates_sandbox.pem")
+        APN.certificate = File.read("/root/Server/campus_de_lunch/config/server_certificates_sandbox.pem")
 
         notification = Houston::Notification.new(device: tokens[params[:inviter]])
         notification.sound = ''
-        notification.alert = "STEP4: Detected Reply. FROM: #{params[:inviter]} TO: #{params[:invitee]}"
+#        notification.alert = "STEP4: Detected Reply. FROM: #{params[:inviter]} TO: #{params[:invitee]}"
         notification.content_available = true
         notification.custom_data = {inviter: params[:inviter], invitee: params[:invitee], status: params[:status]}
 
         APN.push(notification)
         puts "Error: #{notification.error}." if notification.error
 
+        if params[:status] == "1"
+          APN = Houston::Client.development
+          APN.certificate = File.read("/root/Server/campus_de_lunch/config/server_certificates_sandbox.pem")
+          notification = Houston::Notification.new(device: tokens[params[:invitee]])
+          notification.sound = 'default'
+          notification.alert = "#{params[:inviter]}に誘われました！"
+#          notification.content_available = true
+#          notification.custom_data = {inviter: params[:inviter], invitee: params[:invitee], status: params[:status]}
+
+          APN.push(notification)
+          puts "Error: #{notification.error}." if notification.error
+        end
       else
+
+        statusArray.push({inviter: params[:invirter], invitee: params[:invitee], status: params[:status]})
         api_key = "AIzaSyBzrhYXPn3w0fxbARde28W9Z8rg6FI2J_M"
         registration_ids = ["APA91bGw9p-hK7-36QSQyDjpggKuueORzHhKbFeMW5LbH3bWCRYwMzk-8fqXle8Q9XHIczqEiABtLBfTMmUcu-IzKel5SjAS6vHKPLvM-Iqt3KidaMvfXIn7lXaNBVh60j8n_547St9hgajQTinMqtFiSq9pdMM1dg"] # 送りたいregistration_idの配列
         gcm = GCM.new(api_key)
         options = {data: {inviter: params[:invirter], invitee: params[:invitee], status: params[:status]}, collapse_key: "updated_score"}
         response = gcm.send_notification(registration_ids, options)
       end
+    end
+
+    get :status do
+      statusArray
     end
 
   end
